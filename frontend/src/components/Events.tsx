aac2eca68c0587d594f6fb737622ebfa7f8cc8ef
Pauli Matila <pauli.matila@gmail.com>
2020-11-19 14:01:53 +0200

    deleted useless components

--- frontend/src/components/Events.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import EventComponent from '../EventComponent';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const Events = () => {
-  return <EventComponent />;
-};
-
-export default Events;
--- frontend/src/components/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import UserComponent from '../UserComponent';
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const Home = () => {
-  return <UserComponent />;
-};
-
-export default Home;
