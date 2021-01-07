2165ddcd2ad813ea00ee776ada0fa85d63c689ec
Harri V <hvayr@hotmail.com>
2021-01-07 17:26:31 +0200

    hide selectedRowCount

--- frontend/src/components/profile/TestEvents.tsx
+++ frontend/src/components/profile/TestEvents.tsx
@@ -3,6 +3,18 @@ import { address, doFetch, Method, Path } from '../../api/utils';
 import { DataGrid, ColDef } from '@material-ui/data-grid';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { createStyles, Theme } from '@material-ui/core/styles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    test: {
+      '& .MuiDataGrid-selectedRowCount': {
+        fontSize: '0',
+      },
+    },
+  }),
+);
 
 const myEventColums: ColDef[] = [
   { field: 'name', headerName: 'Event category', width: 150 },
@@ -29,6 +41,8 @@ const joinedEventColumns: ColDef[] = [
 export const JoinedEvents: React.FC = () => {
   const [data, setData] = useState([]);
 
+  const classes = useStyles();
+
   useEffect(() => {
     const fetchData = async () => {
       const results = await doFetch(address, Path.EVENTS, Method.GET, false);
@@ -44,8 +58,13 @@ export const JoinedEvents: React.FC = () => {
   }, []);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={data} columns={joinedEventColumns} pageSize={5} />
+    <div style={{ height: 400, width: '100%' }} className={classes.test}>
+      <DataGrid
+        className={classes.test}
+        rows={data}
+        columns={joinedEventColumns}
+        pageSize={5}
+      />
     </div>
   );
 };
@@ -53,6 +72,8 @@ export const JoinedEvents: React.FC = () => {
 export const MyEvents: React.FC = () => {
   const [data, setData] = useState([]);
 
+  const classes = useStyles();
+
   useEffect(() => {
     const fetchData = async () => {
       const results = await doFetch(address, Path.EVENTS, Method.GET, false);
@@ -68,8 +89,13 @@ export const MyEvents: React.FC = () => {
   }, []);
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={data} columns={myEventColums} pageSize={5} />
+    <div style={{ height: 400, width: '100%' }} className={classes.test}>
+      <DataGrid
+        rows={data}
+        columns={myEventColums}
+        pageSize={5}
+        className={classes.test}
+      />
     </div>
   );
 };
