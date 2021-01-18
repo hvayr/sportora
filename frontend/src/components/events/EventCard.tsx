1e91f6d2010a35642d974b1a3afce06449671111
Harri V <hvayr@hotmail.com>
2021-01-18 16:57:04 +0200

    minor fix

--- frontend/src/components/events/EventCard.tsx
+++ frontend/src/components/events/EventCard.tsx
@@ -3,27 +3,28 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import {
-  Button,
-  Collapse,
-  Dialog,
-  Grid,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-} from '@material-ui/core';
+import { Button, Collapse, Grid, IconButton } from '@material-ui/core';
 import { sports } from '../../api/sports';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import moment from 'moment';
 
 const dummyParticipants = [
   {
-    nickname: 'antero',
+    nickname: 'testimies1',
   },
 
   {
-    nickname: 'jorma',
+    nickname: 'testimies2',
+  },
+  {
+    nickname: 'testimies3',
+  },
+
+  {
+    nickname: 'testimies4',
+  },
+  {
+    nickname: 'testimies5',
   },
 ];
 
